40153f1d039f65ec8496fd7ad2b67f0001b123b1
trdwll <git@trdwll.com>
2019-04-24 21:33:41 -0400

    Change DEBUG to False

--- TRDWLL/settings.py
+++ TRDWLL/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pp)6iq_^nw)7k_1%#@v%^srq!zqsvr+ed&rfzfkzi^2+i7h1np'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'trdwll.com']
 
