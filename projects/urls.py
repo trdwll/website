192b4bce54d554306325343dc55ea0ac0a858c40
trdwll <git@trdwll.com>
2019-06-02 15:43:51 -0400

    add lookup of specific build version

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/<slug:slug>/update/<channel>/', ProjectUpdateListView.as_view())
+    path('api/<slug:slug>/update/<str:channel>/', ProjectUpdateListView.as_view()),
+    path('api/<slug:slug>/update/<str:channel>/<str:version>/', ProjectUpdateVersionView.as_view())
 ]
