11482338e522dbaec1c1f29c023181727e8fe624
Russ Treadwell <git@trdwll.com>
2020-01-20 06:14:02 -0500

    force redirect home when /blog/

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import BlogPostView
+from TRDWLL.views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view()), # force redirect back to home
     path('<slug:slug>/', BlogPostView.as_view(), name='blog_post_page'),
 ]
