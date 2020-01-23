299bbdeffc91b1566a29afe0b672865fffd48fd7
Russ Treadwell <git@trdwll.com>
2020-01-23 12:25:37 -0500

    Cleanup context_processors.py again

--- TRDWLL/context_processors.py
+++ TRDWLL/context_processors.py
@@ -2,25 +2,17 @@ from .utils import get_version_checksum
 from blog.models import Post
 from experiments.models import Experiment
 
-def get_posts():
+def get_formatted_data(query):
     archive = {}
 
-    for post in Post.objects.filter(is_published=True).order_by('-published_date'):
-        archive.setdefault(post.published_date.year, []).append(post)
-
-    return archive
-
-def get_experiments():
-    archive = {}
-
-    for experiment in Experiment.objects.filter(is_published=True).order_by('-published_date'):
-        archive.setdefault(experiment.published_date.year, []).append(experiment)
+    for obj in query:
+        archive.setdefault(obj.published_date.year, []).append(obj)
 
     return archive
 
 def global_settings(request):
     return {
         'SITE_VERSION': get_version_checksum(),
-        'POSTS_HOMEPAGE': get_posts(),
-        'EXPERIMENTS': get_experiments()
+        'POSTS_HOMEPAGE': get_formatted_data(Post.objects.filter(is_published=True).order_by('-published_date')),
+        'EXPERIMENTS': get_formatted_data(Experiment.objects.filter(is_published=True).order_by('-published_date'))
     }
