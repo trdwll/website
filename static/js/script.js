3690f14fab158f5e84d6dbf42b0ea39ccc583cd5
Russ Treadwell <git@trdwll.com>
2020-04-12 00:01:01 -0400

    Add some quotes to headline.

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,11 @@ var tags = [
     'Built with Love!',
     'Built with <span class="fa-color-red"><i class="fas fa-heart"></i></span>',
     'Built in the USA!',
+    '"Arguing that you don\'t care about the right to privacy because you have nothing to hide, is no different than saying you don\'t care about free speech because you have nothing to say." - Edward Snowden',
+    'Trust, but verify',
+    '"All our dreams can come true, if we have the courage to pursue them." - Walt Disney',
+    '"Someone is sitting in the shade today because someone planted a tree a long time ago." - Warren Buffett',
+    '"If you can\'t explain it simply, you don\'t understand it well enough." - Albert Einstein',
 ];
 
 $(document).ready(function() {
