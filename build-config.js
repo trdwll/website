18366f1801c8cec791b5a87fb6d3d28c7e802c7b
Russ Treadwell <git@trdwll.com>
2020-10-24 12:42:46 -0400

    fix up some css that's reset via tailwind, change bg color of body, change icons in footer order

---
+++ build-config.js
@@ -0,0 +1,159 @@
+﻿/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/4e3a4ca28d8cb0c407b480601a63d4b6
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/4e3a4ca28d8cb0c407b480601a63d4b6
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'full',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'a11yhelp' : 1,
+		'about' : 1,
+		'ajax': 1, // custom
+		'autoembed': 1, // custom
+		'autogrow': 1, // custom
+		'autolink': 1, // custom
+		'basicstyles' : 1,
+		'bidi' : 1,
+		'bbcode' : 1, // custom
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'codesnippet': 1, // custom
+		'codesnippetgeshi': 1, // custom
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'contextmenu' : 1,
+		'copyformatting' : 1,
+		'devtools': 1, // custom
+		'dialog': 1, // custom
+		'dialogadvtab' : 1,
+		'div' : 1,
+		'divarea': 1, // custom
+		'docprops': 1, // custom
+		'embed': 1, // custom
+		'embedbase': 1, // custom
+		'embedsemantic': 1, // custom
+		'emojione': 1,	// custom
+		'elementspath' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'filetools' : 1, // custom
+		'find' : 1,
+		'flash' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'forms' : 1,
+		'html5video': 1,	// custom
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'iframe' : 1,
+		'iframedialog' : 1, // custom
+		'image' : 1,
+		'image2' : 1, // custom
+		'indentblock' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'language' : 1,
+		'lineutils': 1, // custom
+		'link' : 1,
+		'list' : 1,
+		'liststyle' : 1,
+		'magicline' : 1,
+		'mathjax' : 1, // custom
+		'maximize' : 1,
+		'menubutton' : 1, // custom
+		'newpage' : 1,
+		'notification' : 1, // custom
+		'notificationaggregator' : 1, // custom
+		'pagebreak' : 1,
+		'pastefromgdocs' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'pastetools' : 1,
+		'preview' : 1,
+		'print' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'save' : 1,
+		'scayt' : 1,
+		'selectall' : 1,
+		'sharedspace' : 1, // custom
+		'showblocks' : 1,
+		'showborders' : 1,
+		'smiley' : 1,
+		'sourcearea' : 1,
+		'sourcedialog' : 1, // custom
+		'specialchar' : 1,
+		'stylesheetparser' : 1, // custom
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1, // custom
+		'tableselection' : 1,
+		'tabletools' : 1,
+		'templates' : 1,
+		'textmatch' : 1, // custom
+		'toolbar' : 1,
+		'uicolor' : 1, // custom
+		'undo' : 1,
+		'uploadfile' : 1, // custom
+		'uploadimage' : 1, // custom
+		'uploadwidget' : 1, // custom
+		'widget': 1,	// custom
+		'widgetselection': 1,	// custom
+		'wsc' : 1,
+		'xml' : 1, // custom
+		'wysiwygarea' : 1,
+		'youtube' : 1 // custom
+	},
+	languages : {
+		'en' : 1,
+	}
+};
