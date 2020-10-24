fd83d545903915f5c64c058b435e13f0c9611a0a
Russ Treadwell <git@trdwll.com>
2020-10-24 14:41:48 -0400

    small design fixes

--- experiments/models.py
+++ experiments/models.py
@@ -8,15 +8,26 @@ from django.template.loader import render_to_string
 from ckeditor_uploader.fields import RichTextUploadingField
 from ckeditor.fields import RichTextField
 
-experiment_status = (
-    ('', 'Blank'),
-    ('text-gray-500', 'Unmaintained'),
-    ('text-green-500', 'Active'),
-    ('text-red-500', 'Abandoned'),
-    ('text-blue-500', 'On-Hold'),
-)
+def parse_status_to_string(experiment):
+    if experiment.get_status_display() == 'Unmaintained':
+        return 'This project is currently not being maintained, but will still get updates as needed.'
+    elif experiment.get_status_display() == 'Active':
+        return 'This project is in active development.'
+    elif experiment.get_status_display() == 'Abandoned':
+        return 'This project has been killed and probably will not come back to life. Unless zombies are real?'
+    elif experiment.get_status_display() == 'On-Hold':
+        return 'This project has been put on the back burner for the time being. This doesn\'t mean it\'s been abandoned.'
+
+    return ''
 
 class Experiment(models.Model):
+    experiment_status = (
+        ('', 'Blank'),
+        ('text-gray-500', 'Unmaintained'),
+        ('text-green-500', 'Active'),
+        ('text-red-500', 'Abandoned'),
+        ('text-blue-500', 'On-Hold'),
+    )
     published_date = models.DateTimeField(help_text='When was this experiment created?')
     title = models.CharField(max_length=100, help_text='Title of the post.')
     description = models.CharField(max_length=500, help_text='The description of the experiment.')
@@ -31,6 +42,8 @@ class Experiment(models.Model):
     def get_absolute_url(self):
         return reverse('experiment_post_page', kwargs={'slug': self.slug})
 
+        
+
     def get_experiments_formatted():
         """ Get the posts and format them for display """
         queried_experiments = get_formatted_data(Experiment.objects.filter(is_published=True))
@@ -42,7 +55,7 @@ class Experiment(models.Model):
 
             for experiment in experiments:
                 description = experiment.description[0:117]+'...' if len(experiment.description) > 117 else experiment.description
-                formatted_experiments.append(render_to_string('experiments/extra/list_body.html', {'experiment': experiment, 'description': description}))
+                formatted_experiments.append(render_to_string('experiments/extra/list_body.html', {'experiment': experiment, 'description': description, 'status_message': parse_status_to_string(experiment)}))
             
             # end the row
             formatted_experiments.append(render_to_string('experiments/extra/list_end.html', {}))
