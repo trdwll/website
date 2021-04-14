544f8b8cf03dd5aabea9b3ec506a456e26c67e96
Russ Treadwell <git@trdwll.com>
2021-04-14 19:29:49 -0400

    some styling fixes

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -18,8 +18,7 @@ module.exports = {
   purge: {
     enabled: true, //true for production build
     content: [
-        '../templates/*.html',
-        '../templates/**/*.html'
+      '../templates/**/*.html',
     ],
     options: {
       safelist: ['dark', /^text-(red|blue|gray|green)-500/, /^border-gray-(300|700)/, 'bg-gray-200', 'border-solid', 'border-collapse']
